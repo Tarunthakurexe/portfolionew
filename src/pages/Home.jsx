2edd0de0dc01a8610c5e22d1a7fd3114133c2b25
Tarunthakurexe <106328265+Tarunthakurexe@users.noreply.github.com>
2025-10-08 20:59:10 +0530

    tiny changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
-import { Projects } from "../components/projects";
+import { Projects } from "../components/Projects";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
 
